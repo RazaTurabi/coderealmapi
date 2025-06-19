864ea140a074a18b2f0971540c0406efdea52075
RazaTurabi <razaturabi92@gmail.com>
2025-06-19 19:39:02 +0530

    update phonenumbers countrycode

--- models/formModel.js
+++ models/formModel.js
@@ -15,8 +15,6 @@ class Form {
   }
 
   static async setupTable() {
-  // Drop old table if it exists
-  await db.execute(`DROP TABLE IF EXISTS submissions`);
 
   // Then create fresh table
   await db.execute(`
